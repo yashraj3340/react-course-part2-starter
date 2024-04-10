8b238a05f1927827a7ec59806b9a740e9a3c0453
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-10 14:29:15 +0530

    Paginated queries

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,6 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+interface PostQuery {
+    page: number;
+    pageSize: number;
+  
+}
 interface Post {
     id: number;
     title: string;
@@ -8,19 +13,20 @@ interface Post {
     userId: number;
   }
 
-   const usePosts = (userId: number | undefined) => { 
+   const usePosts = (query : PostQuery) => { 
     const FetchPosts = () =>
         axios
           .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-            params: { userId },
+            params: { _start: (query.page - 1) * query.pageSize, _limit: query.pageSize},
           
           })
           .then((res) => res.data);
     
           return useQuery<Post[], Error>({
-            queryKey:userId? ["users", userId, "posts"] : ["posts"],
+            queryKey:['posts',query],
             queryFn: FetchPosts,
             staleTime: 1000 * 10,
+            keepPreviousData: true,
           });
   }
 
