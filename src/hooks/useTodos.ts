5bf4b5e2ea9700903d3387c59aaea018e6b3d3a3
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-10 12:19:28 +0530

    customizing query settings

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -17,6 +17,7 @@ const useTodos = () => {
           return useQuery<Todo[], Error>({
             queryKey: ["todos"],
             queryFn: FetchTodos,
+            staleTime: 1000 * 10,
           });
 }
 
